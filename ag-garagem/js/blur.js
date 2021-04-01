432bf0b5afd517ded3e1092f9172c707fdf3880f
VictorRibeiro29 <victorribeiro2929@gmail.com>
2021-04-01 11:18:28 -0300

    New files

---
+++ ag-garagem/js/blur.js
@@ -0,0 +1,7 @@
+// const navigationButton = document.getElementById('navigationButton')
+
+// const evaluations = document.querySelector('.evaluations')
+
+// navigationButton.addEventListener('click', () => {
+//     evaluations.classList.toggle('blur')
+// })
