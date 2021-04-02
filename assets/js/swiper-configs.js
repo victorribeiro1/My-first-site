a4a5e840440ec703fb2b4fa945095ea54e00fe3f
VictorRibeiro29 <victorribeiro2929@gmail.com>
2021-04-02 18:53:19 -0300

    Update FAQ

--- assets/js/swiper-configs.js
+++ assets/js/swiper-configs.js
@@ -1,21 +1,19 @@
 var swiper = new Swiper('.swiper-container', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-      rotate: 20,
-      stretch: 00,
-      depth: 0,
-      modifier: 1,
-      slideShadows: true,
-      speed: 1000,
-      slidesPerView: 3,
-      freeMode: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    },
-    loop: true
-  });
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 150,
+    modifier: 1,
+    slideShadows: true,
+    freeMode: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+});
