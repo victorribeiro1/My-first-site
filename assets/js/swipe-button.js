14abb666cbc2f774580d2a51c53741e000ae70b7
victorribeiro1 <victorribeiro2929@gmail.com>
2021-04-18 13:04:26 -0300

    update explore in header

---
+++ assets/js/swipe-button.js
@@ -0,0 +1,5 @@
+const swipeButton = document.getElementById('swipeButton')
+
+swipeButton.addEventListener('click', () => {
+    swipeButton.classList.toggle('active')
+})
--- assets/js/test.js
+++
@@ -1,7 +0,0 @@
-const TotSeconds = 60
-
-const seconds = TotSeconds % 60
-
-const minutos = TotSeconds / 60
-
-console.log(`${TotSeconds} segundos == ${minutos} minutos e ${seconds} segundos.`)
