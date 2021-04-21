34da9b460c1626d82f2ca218837a86adf573115b
victorribeiro1 <victorribeiro2929@gmail.com>
2021-04-21 18:53:30 -0300

    Update Things

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,12 +1,12 @@
 const dropdown = document.querySelector('.dropdown')
 const dropdownMenu = document.querySelector('.dropdown__menu')
-const dropdownItems = document.querySelectorAll('.dropdown__link')
+const dropdownItems = document.querySelectorAll('.dropdown__content-box')
 
 dropdown.addEventListener('mousemove', () => dropdown.classList.add('active'))
 dropdown.addEventListener('mouseout', () => dropdown.classList.remove('active'))
 
 dropdownItems.forEach(item => {
     item.addEventListener('mouseout', () => dropdown.classList.remove('active'))
-    item.addEventListener('mousemove', () => dropdown.classList.add('active'))
+    item.addEventListener('mouseenter', () => dropdown.classList.add('active'))
 })
 
