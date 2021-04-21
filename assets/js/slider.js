82b66bf230d002dbbc1164865dc8a68cc5d2df19
victorribeiro1 <victorribeiro2929@gmail.com>
2021-04-20 21:26:31 -0300

    Update slider.js

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,18 +1,15 @@
 const slider = document.querySelector('.slider')
 
+slider.style.backgroundImage = "url('media/images/black-car-4-d.jpg')"
+
 images = [
     image1 = "url('media/images/black-car-4-d.jpg')",
     image2 = "url('media/images/black-car-2-d.jpg')",
     image3 = "url('media/images/white-car-1-d.jpg')",
 ]
 
-
 counter = 0
 setInterval(() => {
     slider.style.backgroundImage = images[counter]
     counter == 2 ? counter = 0 : counter++
-}, 5000)
-
-// setTimeout(() => {
-//     image = "url('../../media/images/black-car-4-d.jpg')"
-// }, 2000)
+}, 3000)
