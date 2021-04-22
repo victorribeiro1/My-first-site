670287fda914de69d67ee64e61d2e718d4ffacd7
victorribeiro1 <victorribeiro2929@gmail.com>
2021-04-22 07:48:05 -0300

    Update contac.js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,5 +1,4 @@
 const dropdown = document.querySelector('.dropdown')
-const dropdownMenu = document.querySelector('.dropdown__menu')
 const dropdownItems = document.querySelectorAll('.dropdown__content-box')
 
 dropdown.addEventListener('mousemove', () => dropdown.classList.add('active'))
