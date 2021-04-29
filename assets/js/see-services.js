c43977c372ac4fd7148b13ca91ad728fb7fb3b7a
victorribeiro1 <victorribeiro2929@gmail.com>
2021-04-28 23:08:26 -0300

    update services

--- assets/js/see-services.js
+++ assets/js/see-services.js
@@ -1,11 +1,11 @@
 const seeServicesButton = document.getElementById('seeServices')
 const descriptionTexts = document.querySelectorAll('.vertical-carousel__text')
 const serviceBoxes = document.querySelectorAll('.carousel__services-box')
+const services = document.querySelectorAll('.carousel__service')
 
 seeServicesButton.addEventListener('click', () => {
     descriptionTexts.forEach(text => text.classList.toggle('hide'))
     seeServicesButton.classList.toggle('active')
-    serviceBoxes.forEach(box => {
-        box.classList.toggle('active')
-    })
+    serviceBoxes.forEach(box => box.classList.toggle('active'))
+    services.forEach(service => service.classList.toggle('active'))
 })
