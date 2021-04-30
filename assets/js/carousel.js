40e0cf1c17698c5de9451fc3690084479e7ad76b
victorribeiro1 <victorribeiro2929@gmail.com>
2021-04-29 23:21:40 -0300

    Update services

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,12 +1,35 @@
-document.querySelector('.carousel__items')
-.addEventListener('wheel', (e) => slide(e))
+const allItems = document.querySelectorAll('.carousel__item')
+const allServices = document.querySelectorAll('.carousel__service')
+const servicesSection = document.querySelector('.services')
+const items = document.querySelector('.carousel__items')
+
+// lembre-se de usar o magic scroll para usar 
+// a funcionalidade do slide apenas quando a section estiver no meio da tela
+
+// const firstItem = allItems[0]
+
+console.log(items)
+
+//counter has already been decleared before
+let counter2 = 0
 
 function slide(e) {
-    console.log('scrolled')
     if (e.deltaY > 0) {
-        return e.target.scrollBy(900, 0)
+        // console.log('scrol up')
+        
+        allItems.forEach(item => item.classList.add(`slideTo${++counter2}`))
+        servicesSection.classList.add('noScroll')
+        // items.scrollBy(900, 0)
+        
+        
+        // return e.target.scrollBy(900, 0)
     }
     else {
-        return e.target.scrollBy(-900, 0)
+        console.log('scrol down')
+        items.scrollBy(-900, 0)
+
+        // allItems.forEach(item => item.classList.remove(`slideTo${++counter2}`))
     }
 }
+
+servicesSection.addEventListener('wheel', (e) => slide(e))
--- assets/js/see-services.js
+++ assets/js/see-services.js
@@ -1,11 +1,33 @@
+// This file is response for the menegement of the display at services inside the carousel
+
 const seeServicesButton = document.getElementById('seeServices')
 const descriptionTexts = document.querySelectorAll('.vertical-carousel__text')
 const serviceBoxes = document.querySelectorAll('.carousel__services-box')
 const services = document.querySelectorAll('.carousel__service')
 
-seeServicesButton.addEventListener('click', () => {
-    descriptionTexts.forEach(text => text.classList.toggle('hide'))
-    seeServicesButton.classList.toggle('active')
-    serviceBoxes.forEach(box => box.classList.toggle('active'))
-    services.forEach(service => service.classList.toggle('active'))
-})
+function toggleClasses() {
+    descriptionTexts.forEach(text => text.classList.toggle('hide')) // Opacity of the text
+    seeServicesButton.classList.toggle('active') // Animation of the button
+    serviceBoxes.forEach(box => box.classList.toggle('active')) // Background blur
+    services.forEach(service => service.classList.toggle('active')) // Animation of the services -> translateX and Opacity
+}
+
+function addClasses() {
+    descriptionTexts.forEach(text => text.classList.add('hide'))
+    seeServicesButton.classList.add('active')
+    serviceBoxes.forEach(box => box.classList.add('active'))
+    services.forEach(service => service.classList.add('active'))
+}
+
+function removeClasses() {
+    descriptionTexts.forEach(text => text.classList.remove('hide'))
+    seeServicesButton.classList.remove('active')
+    serviceBoxes.forEach(box => box.classList.remove('active'))
+    services.forEach(service => service.classList.remove('active'))
+}
+
+// seeServicesButton.addEventListener('click', () => toggleClasses())
+seeServicesButton.addEventListener('mouseenter', toggleClasses)
+serviceBoxes.forEach(box => box.addEventListener('mouseenter', addClasses))
+services.forEach(service => service.addEventListener('mouseout',  removeClasses))
+services.forEach(service => service.addEventListener('mousemove', addClasses))
