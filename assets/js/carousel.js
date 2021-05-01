e6dbf6ebc1fca4ec1743a3bf73b1828d8830d150
victorribeiro1 <victorribeiro2929@gmail.com>
2021-05-01 00:10:33 -0300

    Update services

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -6,30 +6,47 @@ const items = document.querySelector('.carousel__items')
 // lembre-se de usar o magic scroll para usar 
 // a funcionalidade do slide apenas quando a section estiver no meio da tela
 
-// const firstItem = allItems[0]
-
-console.log(items)
-
-//counter has already been decleared before
-let counter2 = 0
-
 function slide(e) {
     if (e.deltaY > 0) {
-        // console.log('scrol up')
-        
-        // allItems.forEach(item => item.classList.add(`slideTo${++counter2}`))
-        // servicesSection.classList.add('noScroll')
         items.scrollBy(900, 0)
-        
-        
-        // return e.target.scrollBy(900, 0)
     }
     else {
-        // console.log('scrol down')
         items.scrollBy(-900, 0)
-
-        // allItems.forEach(item => item.classList.remove(`slideTo${++counter2}`))
     }
 }
 
 servicesSection.addEventListener('wheel', (e) => slide(e))
+
+// CAROUSEL MANAGER ->
+
+const carouselButton = document.querySelectorAll('.carousel-button')
+
+function clickShadow(event) {
+    const target = event.target
+
+    let x = event.clientX - event.target.offsetLeft
+    let y = event.clientY - event.target.offsetTop
+    console.log(event.target.offsetTop)
+
+    let ripples = document.createElement('span')
+    ripples.style.left = `${x}px`
+    ripples.style.top = `${y}px`
+
+    target.appendChild(ripples)
+}
+
+carouselButton.forEach(button => {
+    button.addEventListener('click', function(event) {
+        const target = event.target
+
+        let x = event.clientX - event.target.offsetLeft
+        let y = event.clientY - event.target.offsetTop
+        console.log(event.target)
+
+        let ripples = document.createElement('span')
+        ripples.style.left = `${x}px`
+        ripples.style.top = `${y}px`
+
+        this.appendChild(ripples)
+    })
+})
