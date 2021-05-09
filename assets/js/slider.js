79216cfce596d6de52011491980a201fd73796bd
victorribeiro1 <victorribeiro2929@gmail.com>
2021-05-09 14:57:35 -0300

    Update slide.js

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,11 +1,19 @@
 const slider = document.querySelector('.slider')
 
-let counter = 1
+const imagesPosition = ['first', 'second', 'tertiary']
+
+let imageIndex = 0
+
+function setNextIndex() {
+    imageIndex++
+    imageIndex = imageIndex > imagesPosition.length - 1 ? imageIndex = 0 : imageIndex
+}
+
+function loadImage() {
+    slider.setAttribute('data-slide', imagesPosition[imageIndex])
+}
+
 setInterval(() => {
-    slider.classList.remove(`image--${counter}`)
-    slider.classList.remove(`image--3`)
-
-    slider.classList.add(`image--${++counter}`)
-    counter = counter >= 3 ? counter = 0 : counter
-
-}, 3000)
+    setNextIndex()
+    loadImage()
+}, 2800)
