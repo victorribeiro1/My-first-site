8d311b9d138770adf5d143f695cc09f0bfc51e45
victorribeiro1 <victorribeiro2929@gmail.com>
2021-05-24 12:15:53 -0300

    Revert "udpate"
    This reverts commit e7e0342f8dc6ccfc81cecc7a63691bb788064fc5.

---
+++ assets/js/button-ripple-effect.js
@@ -0,0 +1,21 @@
+// const carouselButtons = document.querySelectorAll(".carousel-button")
+
+// carouselButtons.forEach( button => {
+//     button.addEventListener('click', function(e) {
+//         let x = e.clientX - e.target.offsetLeft
+//         let y = e.clientY - e.target.offsetTop
+
+//         let ripple = document.createElement('span')
+//         ripple.style.left = x + 'px'
+//         ripple.style.top = x + 'px'
+
+//         this.appendChild(ripple)
+
+//         console.log(e.target.offsetTop)
+//     })
+// })
+
+
+// window.addEventListener('click', (e) => {
+//     // console.log(e.clientX)
+// })
