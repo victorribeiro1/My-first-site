5f9d6ec05b588e25ffc1820696a03960a88a80d7
victorribeiro1 <victorribeiro2929@gmail.com>
2021-06-02 11:05:52 -0300

    Update toggle menu

--- assets/js/toggle-menu.js
+++ assets/js/toggle-menu.js
@@ -1,6 +1,7 @@
 const toggleMenu = document.querySelector('.toggle-menu')
 const toggleMenuButton = document.querySelector('.toggle-menu__icon-box')
 const htmlTag = document.querySelector('html')
+const Alllinks = document.querySelectorAll('a.link')
 
 // block scroll when toggle-menu is oppened
 // encrease the z-index to hide navigation
@@ -8,3 +9,12 @@ toggleMenuButton.addEventListener('click', () => {
     htmlTag.classList.toggle('menu-is-active')
     toggleMenu.classList.toggle('active')
 }) 
+
+Alllinks.forEach(link => {
+    link.addEventListener('click', () => {
+        console.log('clicked')
+        htmlTag.classList.remove('menu-is-active')
+        toggleMenu.classList.remove('active')
+        document.getElementById('toggle').checked = false
+    })
+})
