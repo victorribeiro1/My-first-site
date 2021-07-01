5df1a3ea03d4479bd21831e86c6c69927725a369
victorribeiro1 <victorribeiro2929@gmail.com>
2021-07-01 15:00:06 -0300

    update footer-mobile script

---
+++ assets/js/footer-mobile.js
@@ -0,0 +1,7 @@
+const linksBox = document.querySelectorAll('.footer-mobile__links-box')
+
+linksBox.forEach(boxLink => {
+    boxLink.addEventListener('click', () => {
+        boxLink.classList.toggle('active')
+    })
+})
