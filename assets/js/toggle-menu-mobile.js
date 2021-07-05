6c93b99cefac17e440727ba524d179f2726d892f
victorribeiro1 <victorribeiro2929@gmail.com>
2021-07-05 18:35:23 -0300

    update

--- assets/js/toggle-menu-mobile.js
+++ assets/js/toggle-menu-mobile.js
@@ -4,3 +4,19 @@ const toggleMenuMobileButton = document.querySelector('.toggle-menu-mobile__butt
 toggleMenuMobileButton.addEventListener('click', () => {
     toggleMenuMobile.classList.toggle('active')
 })
+
+// BEFORE POSITION 
+
+const TMBheadings = document.querySelectorAll('.toggle-menu-mobile__heading')
+const TMBheadingBox = document.querySelector('.toggle-menu-mobile__headings-box')
+
+TMBheadings.forEach(TMBheading => {
+    TMBheading.addEventListener('click', e => {
+        if (e.target.classList.contains('right')) {
+            TMBheadingBox.classList.add('right')
+        }
+        else {
+            TMBheadingBox.classList.remove('right')
+        }
+    })
+})
